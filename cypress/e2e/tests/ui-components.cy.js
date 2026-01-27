4416174cd88a06a6a778af5460ad099113fcddf8
Pawel Duminski <pduminski@gmail.com>
2026-01-27 16:26:31 +0100

    Input fields with key presses

--- cypress/e2e/tests/ui-components.cy.js
+++ cypress/e2e/tests/ui-components.cy.js
@@ -8,9 +8,23 @@ it('Input Fields', () => {
     cy.contains('Forms').click();
     cy.contains('Form Layouts').click();
 
-    cy.get('#inputEmail1').type('test@test.com', { delay: 200 }).clear().type('Hello');
-
-    cy.contains('nb-card', 'Using the Grid').contains('Email').type("' Yes it works'");
+    const name = 'TestUser';
+    cy.get('#inputEmail1').type('test@test.com', { delay: 200 }).clear()
+    cy.contains('nb-card', 'Using the Grid').contains('Email').type(name + '@test.com');
 
     // .clear will not work on the label -> thicky thing 
+
+    cy.get('#inputEmail1').should('have.value', name + '@test.com').clear().type('test@test.com');
+})
+
+it.only('Input fields and key presses', () => {
+    cy.contains('Auth').click();
+    cy.contains('Login').click();
+
+    cy.get('#input-email').type('test@test.com');
+    cy.get('#input-password').type('test1234{enter}');
+
+    // TAB is the only modifier that does not work 
+    // cy.press will support that command 
+    // cy.press(Cypress.Keyboard.Keys.TAB)
 })
